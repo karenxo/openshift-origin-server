43c1a605f31cdfcfc61aefaa19b227873d1fd827
Filirom1 <filirom1@gmail.com>
2013-06-12 14:59:22 +0000

    merge

--- broker/config/environments/test.rb
+++ broker/config/environments/test.rb
@@ -91,7 +91,8 @@ Broker::Application.configure do
     :allow_ha_applications => conf.get_bool('ALLOW_HA_APPLICATIONS', "false"),
     :router_hostname => conf.get('ROUTER_HOSTNAME', "www.example.com"),
     :valid_ssh_key_types => OpenShift::Controller::Configuration.parse_list(conf.get('VALID_SSH_KEY_TYPES', nil)),
-    :allow_obsolete_cartridges => conf.get_bool('ALLOW_OBSOLETE_CARTRIDGES', "false")
+    :allow_obsolete_cartridges => conf.get_bool('ALLOW_OBSOLETE_CARTRIDGES', "false"),
+    :ssh_fqdn => conf.get("SSH_FQDN", "true")
   }
 
   config.auth = {
